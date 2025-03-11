f5376c862d67a389eb34407e433bc9a6e5a2ab22
Alitey <alyalu004@gmail.com>
2025-03-12 01:33:01 +0300

    hot fix: change logs

--- first/app/utils/mood_determinant.py
+++ first/app/utils/mood_determinant.py
@@ -55,8 +55,6 @@ async def mood_determination(user_id, image_file):
 
         if run.status == "completed":
             messages = await client.beta.threads.messages.list(thread_id=thread_id)
-            logger.warning(f"Messages retrieved from thread: {messages}")
-
             assistant_messages = [
                 msg for msg in messages.data if msg.role == "assistant"
             ]
@@ -66,7 +64,6 @@ async def mood_determination(user_id, image_file):
 
             last_message = assistant_messages[0]
             answer = last_message.content[0].text.value.strip()
-            logger.info(f"Mood determined: {answer}")
             return answer
         else:
             logger.error(f"Run failed with status: {run.status}")
@@ -75,47 +72,3 @@ async def mood_determination(user_id, image_file):
     except Exception as e:
         logger.exception(f"Error in mood_determination for user {user_id}: {e}")
         raise
-# async def mood_determination(user_id, image_file):
-#     client = settings.get_ai_settings()
-#     redis_client = settings.get_thread_db()
-#     assistant_id = settings.get_assistant()
-    
-#     thread_id = await get_thread(user_id)
-#     if not thread_id: 
-#         thread = await client.beta.threads.create()
-#         redis_client.set(f"user:{user_id}:thread_id", thread.id)
-#         thread_id = thread.id
-
-#     await client.beta.threads.messages.create(
-#         thread_id=thread_id,
-#         role="user",
-#         content=[
-#             {
-#                 "type": "text",
-#                 "text" : "Determine the mood from the photo. Answer as briefly as possible, preferably in one or more words."
-#             },
-#             {
-#                 "type": "image_file",
-#                 "image_file": {
-#                     "file_id": await upload_photo(image_file, user_id)
-#                 }
-#             }
-#         ]
-#     )
-
-#     run = await client.beta.threads.runs.create_and_poll(
-#         thread_id=thread_id,
-#         assistant_id=assistant_id
-#     )
-
-#     if run.status == "completed":
-#         messages = await client.beta.threads.messages.list(thread_id=thread_id)
-#         logging.warning(messages)
-#         assistant_messages = [
-#             msg for msg in messages.data if msg.role == "assistant"
-#         ]
-#         last_message = assistant_messages[0]
-#         answer = last_message.content[0].text.value.strip()
-#         return answer
-#     else:
-#         raise Exception(f"Run failed with status: {run.status}")
